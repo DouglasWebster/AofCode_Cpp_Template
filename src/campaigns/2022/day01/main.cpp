ebe7401e7c1c42347391a8bae3f6d7ab7a28f485
douglas <douglas.webster2107@gmail.com>
2023-09-09 16:47:35 +0100

    Scaffolded initial files for day01 of year 2022

---
+++ src/campaigns/2022/day01/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "2022_day01.hpp"
+
+int main() {
+    std::cout << "Advent of Code " << AofCode_Cpp::cmake::campaign_year << " " << AofCode_Cpp::cmake::campaign_day << '\n';
+    std::cout << "Part 1 solution:\n";
+    std::cout << "Part 2 solution:\n";
+
+    return 0;
+}
