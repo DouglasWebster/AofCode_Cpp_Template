6e4cdb18f5425cb285d265e9af565888abda7dd0
douglas <douglas.webster2107@gmail.com>
2023-09-12 19:17:19 +0100

    scaffolded AoC year 2022 day 1

--- src/campaigns/2022/day01/main.cpp
+++ src/campaigns/2022/day01/main.cpp
@@ -2,10 +2,12 @@
 
 #include "2022_day01.hpp"
 
-int main() {
-    std::cout << "Advent of Code " << AofCode_Cpp::cmake::campaign_year << " " << AofCode_Cpp::cmake::campaign_day << '\n';
-    std::cout << "Part 1 solution:\n";
-    std::cout << "Part 2 solution:\n";
+int main()
+{
+  std::cout << "Advent of Code " << AofCode_Cpp::cmake::campaign_year << " " << AofCode_Cpp::cmake::campaign_day
+            << '\n';
+  std::cout << "Part 1 solution:\n";
+  std::cout << "Part 2 solution:\n";
 
-    return 0;
+  return 0;
 }
